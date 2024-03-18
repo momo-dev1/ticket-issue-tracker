ba50242536bd61c2d422a367a8aebb5ab05f26b8
momo-dev1 <didy1234567@gmail.com>
2024-03-18 21:16:20 +0200

    Refactoring coding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from "tailwindcss"
 
 const config = {
-  safelist: ['dark'],
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-  ],
+	],
   prefix: "",
   theme: {
     container: {
