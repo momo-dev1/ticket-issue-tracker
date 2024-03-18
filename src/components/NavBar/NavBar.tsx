4b514d2bb52e07bae246597a02a2238323a08ad8
momo-dev1 <didy1234567@gmail.com>
2024-03-18 20:45:42 +0200

    feat: adding dark themes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -36,6 +36,7 @@ const NavBar = () => {
           </nav>
 
           <div className='flex items-center gap-4'>
+            <DarkModeToggle />
             <div className='sm:flex sm:gap-4'>
               <Link
                 className='block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700'
@@ -52,7 +53,6 @@ const NavBar = () => {
               </Link>
             </div>
 
-            {/* <DarkModeToggle /> */}
             <button className='block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden'>
               <span className='sr-only'>Toggle menu</span>
               <svg
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "tailwindcss"
 
 const config = {
+  safelist: ['dark'],
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
