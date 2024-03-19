b0fa4234250eeb5c0ecda64ac0442bb9d305be80
momo-dev1 <didy1234567@gmail.com>
2024-03-19 19:36:55 +0200

    feat: order ticktes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import DataTable from '@/components/DataTable';
 import prisma from '@/lib/prismadb';
 
 const Ticket = async () => {
-  const tickets = await prisma.ticket.findMany();
+  const tickets = await prisma.ticket.findMany({orderBy: [{createdAt: 'desc'}]});
   return (
     <div>
       <DataTable tickets={tickets} />
