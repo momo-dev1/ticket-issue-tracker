ce8377b3d7b1d4721fd3742537f12348f0cc14f3
momo-dev1 <didy1234567@gmail.com>
2024-03-26 20:11:14 +0200

    feat: adding filter status functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,49 @@
 import DataTable from '@/components/DataTable'
 import Pagination from '@/components/Pagination'
+import StatusFilter from '@/components/StatusFilter'
 import { Button } from '@/components/ui/button'
 import prisma from '@/lib/prismadb'
+import { Status } from '@prisma/client'
 import Link from 'next/link'
 
 interface IProps {
+  status: Status
   page: string
 }
 const Ticket = async ({ searchParams }: { searchParams: IProps }) => {
   const pageSize = 5
   const page = parseInt(searchParams.page) || 1
-  const ticketCount = await prisma.ticket.count()
+  const statuses = Object.values(Status)
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined
+
+  let where = {}
+  if (status) {
+    where = { status }
+  } else {
+    where = {
+      NOT: [{ status: 'CLOSED' }],
+    }
+  }
+
+  const ticketCount = await prisma.ticket.count({ where })
 
   const tickets = await prisma.ticket.findMany({
+    where,
     orderBy: [{ createdAt: 'desc' }],
     take: pageSize,
     skip: (page - 1) * pageSize,
   })
   return (
     <>
-      <Button className='bg-gradient-to-r from-cyan-500 to-teal-500'>
-        <Link href='/tickets/new'>Create ticket</Link>
-      </Button>
+      <div className='flex justify-between'>
+        <Button className='bg-gradient-to-r from-cyan-500 to-teal-500'>
+          <Link href='/tickets/new'>Create ticket</Link>
+        </Button>
+        <StatusFilter />
+      </div>
+
       <DataTable tickets={tickets} />
       <Pagination
         ticketCount={ticketCount}
---
+++ src/components/StatusFilter.tsx
@@ -0,0 +1,50 @@
+'use client'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+
+type Status = {
+  label: string
+  value?: string
+}
+const statuses: Status[] = [
+  { label: 'Open / Started' },
+  { label: 'Open', value: 'OPEN' },
+  { label: 'Started', value: 'STARTED' },
+  { label: 'Closed', value: 'CLOSED' },
+]
+
+const StatusFilter = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  return (
+    <Select
+      defaultValue={searchParams.get('status') || ''}
+      onValueChange={(status) => {
+        const params = new URLSearchParams()
+        if (status) params.append('status', status)
+        const query = params.size ? `?${params.toString()}` : '0'
+        router.push('/' + query)
+      }}
+    >
+      <SelectTrigger className='w-[180px]'>
+        <SelectValue placeholder='Filter by status...' />
+      </SelectTrigger>
+      <SelectContent>
+        {statuses.map((status) => (
+          <SelectItem key={status.label || '0'} value={status.value || '0'}>
+            {status.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
+}
+
+export default StatusFilter
